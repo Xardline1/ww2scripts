2f1a73c52b5a89b099a0b6c2bc742251b7e7d630
Xardline1 <132019219+Xardline1@users.noreply.github.com>
2023-12-20 14:01:33 +0300

    Update Autobids_and_Autotimer.user.js

--- Autobids_and_Autotimer.user.js
+++ Autobids_and_Autotimer.user.js
@@ -852,7 +852,7 @@
                       location.reload();
                     }, 3600000);
                 } else {
-                  match = currentTimeLeft.match(/(\d+)\s+ч\.\s+(\d+)\s+м\./);
+                  match = currentTimeLeft.match(/(\d+)\s+ч\.\s+(\d+)\s+мин\./);
                   if (match) {
                     setTimeout(function () {
                       location.reload();
