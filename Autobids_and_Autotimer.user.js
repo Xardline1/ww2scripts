05b21f2ec665b46463a6dbe8df29bb1fba016054
Xardline1 <132019219+Xardline1@users.noreply.github.com>
2023-12-21 11:44:11 +0300

    Update Autobids_and_Autotimer.user.js

--- Autobids_and_Autotimer.user.js
+++ Autobids_and_Autotimer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autobids_and_Autotimer
 // @namespace    http://tampermonkey.net/
-// @version      3.6
+// @version      3.7
 // @description  Auto bids on auction ww2 and auto update the timer
 // @updateURL https://github.com/Xardline1/ww2scripts/raw/main/Autobids_and_Autotimer.user.js
 // @downloadURL https://github.com/Xardline1/ww2scripts/raw/main/Autobids_and_Autotimer.user.js
@@ -292,15 +292,15 @@
             <p>Основной лот:</p>
             <label for="firstBid">
                 Первая ставка:
-                <div class="tooltip-icon" id="firstBidTooltipIcon" tooltip="Устанавливается на 9-ой секунде аукциона">?</div>
-                <div class="tooltip" id="firstBidTooltip">Устанавливается на 9-ой секунде аукциона</div>
+                <div class="tooltip-icon" id="firstBidTooltipIcon" tooltip="Устанавливается на 15-ой секунде аукциона">?</div>
+                <div class="tooltip" id="firstBidTooltip">Устанавливается на 15-ой секунде аукциона</div>
             </label>
             <input type="text" id="firstBid" value="${localStorage.getItem('firstBid') || ''}" placeholder="Пример: 1000">
             <br>
             <label for="secondBid">
                 Вторая ставка:
-                <div class="tooltip-icon" id="secondBidTooltipIcon" tooltip="Устанавливается на 2-ой секунде аукциона">?</div>
-                <div class="tooltip" id="secondBidTooltip">Устанавливается на 2-ой секунде аукциона</div>
+                <div class="tooltip-icon" id="secondBidTooltipIcon" tooltip="Устанавливается на 3-ей секунде аукциона">?</div>
+                <div class="tooltip" id="secondBidTooltip">Устанавливается на 3-ей секунде аукциона</div>
             </label>
             <input type="text" id="secondBid" value="${localStorage.getItem('secondBid') || ''}" placeholder="Пример: 3500">
             <br>
@@ -649,7 +649,7 @@
                         var minutes = parseInt(match[1]);
                         var seconds = parseInt(match[2]);
 
-                        if (minutes === 0 && seconds <= 9) {
+                        if (minutes === 0 && seconds <= 15) {
                             placeBid(firstBid);
                             localStorage.setItem('firstBidPlaced', 'true');
                             clearInterval(intervalId);
@@ -718,7 +718,7 @@
                         var minutes = parseInt(match[1]);
                         var seconds = parseInt(match[2]);
 
-                        if (minutes === 0 && seconds <= 2) {
+                        if (minutes === 0 && seconds <= 3) {
                             placeBid(secondBid);
                             localStorage.setItem('secondBidPlaced', 'true');
                             clearInterval(intervalId);
