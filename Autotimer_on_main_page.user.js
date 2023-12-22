c3068739c6ed16c228e314d30f55a3f25d3f09aa
Xardline1 <132019219+Xardline1@users.noreply.github.com>
2023-12-22 06:10:07 +0300

    Update Autotimer_on_main_page.user.js

--- Autotimer_on_main_page.user.js
+++ Autotimer_on_main_page.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Autotimer_on_main_page
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  Auto update timers on Main Page
-// @updateURL https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
-// @downloadURL https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
+// @updateURL    https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
+// @downloadURL  https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
 // @author       Xardline
 // @match        https://ww1.ru/auctions*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
