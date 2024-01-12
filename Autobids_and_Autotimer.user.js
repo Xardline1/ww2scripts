43ac135602041e0360fc06500c497e7104a8fb38
Xardline1 <132019219+Xardline1@users.noreply.github.com>
2024-01-12 14:49:42 +0300

    Update Autobids_and_Autotimer.user.js

--- Autobids_and_Autotimer.user.js
+++ Autobids_and_Autotimer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autobids_and_Autotimer
 // @namespace    http://tampermonkey.net/
-// @version      4.6
+// @version      4.7
 // @description  Auto bids on auction ww2 and auto update the timer
 // @updateURL    https://github.com/Xardline1/ww2scripts/raw/main/Autobids_and_Autotimer.user.js
 // @downloadURL  https://github.com/Xardline1/ww2scripts/raw/main/Autobids_and_Autotimer.user.js
