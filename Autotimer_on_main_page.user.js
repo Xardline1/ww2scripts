a0e2411384413dff0b089483ad351ad3844a241d
Xardline1 <132019219+Xardline1@users.noreply.github.com>
2024-12-03 17:14:40 +0300

    Update Autotimer_on_main_page.user.js

--- Autotimer_on_main_page.user.js
+++ Autotimer_on_main_page.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autotimer_on_main_page
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Auto update timers on Main Page
 // @updateURL    https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
 // @downloadURL  https://github.com/Xardline1/ww2scripts/raw/main/Autotimer_on_main_page.user.js
@@ -15,7 +15,7 @@
     'use strict';
 
     function updateElementStyle() {
-        var contentWrapElement = document.querySelector('.b-filter-lot__content-wrap');
+        var contentWrapElement = document.querySelector('.b-filter-lot__content');
 
         if (contentWrapElement) {
             if (window.matchMedia('(min-width: 671px) and (max-width: 950px)').matches) {
@@ -27,14 +27,12 @@
     updateElementStyle();
 
     function updateAuctionTime() {
-        // Поиск всех элементов с временем до завершения на странице аукционов
-        var timeLeftElements = document.querySelectorAll('.b-filter-lot__content-vendor span');
+        var timeLeftElements = document.querySelectorAll('.b-filter-lot__reason span');
 
         timeLeftElements.forEach(function(timeLeftElement) {
             var currentTimeLeft = timeLeftElement.innerText.trim();
             var match;
 
-                // формат минут и секунд
             match = currentTimeLeft.match(/(\d+)\s+мин\.\s+(\d+)\s+сек\./);
             if (match) {
                 var minutes = parseInt(match[1]);
@@ -49,9 +47,8 @@
 
                 var newTimeLeft = minutes + ' мин. ' + seconds + ' сек.';
 
-                timeLeftElement.innerHTML = '<span style="color:#ff0000">До завершения - ' + newTimeLeft + '</span>';
+                timeLeftElement.innerHTML = '<span style="color:#ff0000">До завершения ' + newTimeLeft + '</span>';
             } else {
-                // другие форматы
                 match = currentTimeLeft.match(/(\d+)\s+д\.\s+(\d+)\s+ч\./);
                 if (match) {
                     setTimeout(function () {
